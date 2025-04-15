f69b8cb5cc80b8d9693e6f9785beaec370c49481
Caio <110123633+CBarrosoBRRJ@users.noreply.github.com>
2025-04-15 15:32:54 -0300

    Update pipeline.py

--- projeto-kobe-ml/tests/pipelines/separacao_treino_teste/pipeline.py
+++ projeto-kobe-ml/tests/pipelines/separacao_treino_teste/pipeline.py
@@ -1,17 +1,1 @@
-# src/projeto_kobe_ml/pipelines/separacao_treino_teste/pipeline.py
-"""
-Pipeline 'separacao_treino_teste' para dividir os dados em treino e teste
-"""
 
-from kedro.pipeline import node, Pipeline
-from .nodes import split_train_test
-
-def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([
-        node(
-            func=split_train_test,
-            inputs="dados_tratados",
-            outputs=["dados_treino", "dados_teste"],
-            name="split_train_test_node",
-        ),
-    ])
