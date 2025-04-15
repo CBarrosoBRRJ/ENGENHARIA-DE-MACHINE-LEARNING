7f6358a3935a52cd5b4eade91d2f61e732363087
Caio <110123633+CBarrosoBRRJ@users.noreply.github.com>
2025-04-15 15:33:27 -0300

    Update pipeline.py

--- projeto-kobe-ml/tests/pipelines/tratamento_dados/pipeline.py
+++ projeto-kobe-ml/tests/pipelines/tratamento_dados/pipeline.py
@@ -1,17 +1,1 @@
-# src/projeto_kobe_ml/pipelines/tratamento_dados/pipeline.py
-"""
-Pipeline 'tratamento_dados' para processamento do dataset Kobe
-"""
 
-from kedro.pipeline import node, Pipeline
-from .nodes import processar_dados
-
-def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([
-        node(
-            func=processar_dados,
-            inputs={"data": "dataset_kobe_dev"},
-            outputs="dados_tratados",
-            name="tratamento_dados_node",
-        ),
-    ])
